e4e82471c69c10bf6d3439aa79a863f35095f317
sahodar <sahodardhungana@gmail.com>
2019-04-24 11:05:16 +0545

    version 4 with new pages

--- js/my.js
+++ js/my.js
@@ -1,9 +1,11 @@
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i=0;i<3;i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); }
-      next.children(':first-child').clone().appendTo($(this)); } });
+$('.dropdown-submenu > a').on("click", function(e) {
+  var submenu = $(this);
+  $('.dropdown-submenu .dropdown-menu').removeClass('show');
+  submenu.next('.dropdown-menu').addClass('show');
+  e.stopPropagation();
+});
+
+$('.dropdown').on("hidden.bs.dropdown", function() {
+  // hide any open menus when parent closes
+  $('.dropdown-menu.show').removeClass('show');
+});
