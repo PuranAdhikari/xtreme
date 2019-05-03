e0873f3fbd61ae35c6a4df6de61a8c2aac7cea92
sahodar <sahodardhungana@gmail.com>
2019-05-03 18:49:56 +0545

    issues resolved - v6 addded

--- js/my.js
+++ js/my.js
@@ -9,3 +9,19 @@ $('.dropdown').on("hidden.bs.dropdown", function() {
   // hide any open menus when parent closes
   $('.dropdown-menu.show').removeClass('show');
 });
+$(document).ready(function(){
+let v = $('.carousel-main .carousel-inner .carousel-item.active img').attr("src");
+$("#j-main").css({
+  'background-image': 'url('+v+')'  
+});
+});
+$(".carousel-main .controls a").click(function(){
+  if($('.carousel-main .carousel-inner .carousel-item').hasClass("active"))
+  {
+    let v = $('.carousel-main .carousel-inner .carousel-item').next('.active .col-12 .card img').attr("src");
+    console.log(v);
+    $("#j-main").css({
+      'background-image': 'url('+v+')'  
+    });
+  }
+});
